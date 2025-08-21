8d46f9e0fe0880265afb867177de68d7f543d641
MLeertouwer <melissa@melsus.nl>
2025-08-21 10:02:05 +0200

    fixed standalone component + updating angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { QuizComponent } from "./quiz-component/quiz-component.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, QuizComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [QuizComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'quiz-app';
--- src/app/topbtn/topbtn.component.ts
+++ src/app/topbtn/topbtn.component.ts
@@ -2,27 +2,28 @@ import { CommonModule } from '@angular/common';
 import { Component, HostListener, OnInit, } from '@angular/core';
 
 @Component({
-    selector: 'app-topbtn',
-    imports: [CommonModule],
-    templateUrl: './topbtn.component.html',
-    styleUrl: './topbtn.component.css'
+  selector: 'app-topbtn',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './topbtn.component.html',
+  styleUrl: './topbtn.component.css'
 })
-export class TopbtnComponent implements OnInit{
-   showTopButton = false;
+export class TopbtnComponent implements OnInit {
+  showTopButton = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  const windowHeight = window.innerHeight;
-  const documentHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
 
-  // Show the button only if the scroll position exceeds a fraction of the page height
-  this.showTopButton = scrollPosition > (documentHeight - windowHeight) * 0.98;
+    // Show the button only if the scroll position exceeds a fraction of the page height
+    this.showTopButton = scrollPosition > (documentHeight - windowHeight) * 0.98;
   }
 
   scrollToTop() {
     window.scrollTo({ top: 80, behavior: 'smooth' });
   }
-     ngOnInit() {}
-  }
+  ngOnInit() { }
+}
 
